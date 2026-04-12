b3a7a560ee59907840f8d7a3360414b2477e5fd5
BatterySmooth <matt-booth@live.co.uk>
2026-04-12 22:34:43 +0100

    folder path update

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,12 @@ import { useUser } from '@core/useUser';
 import { supabase } from '@core/SupabaseClient';
 import { DrawCanvas } from '@components/DrawCanvas'
 import SupabaseLogin from '@components/SupabaseLogin';
-import '@/App.css'
-
-export type UploadResult = {
-  path: string;
-  url: string;
-};
-
-type UploadFn = (file: File) => Promise<UploadResult>;
+import '@/App.css';
 
 export default function App() {
   const user = useUser();
 
-  const uploadFile: UploadFn = async (file: File) => {
-    // Ensure user is logged in
-    const { data: userData } = await supabase.auth.getUser();
-    const user = userData.user;
+  const uploadFile = async (file: File) => {
     if (!user) {
       alert("User not authenticated");
       throw new Error("User not authenticated");
@@ -29,23 +19,15 @@ export default function App() {
       throw new Error("Only SVG files are allowed");
     }
     // Upload to Supabase Storage
-    const { data, error } = await supabase.storage
+    const { error } = await supabase.storage
       .from(Config.BucketName)
-      .upload(file.name, file, {
+      .upload(`${Config.UploadFolder}/${file.name}`, file, {
         contentType: "image/svg+xml",
         upsert: false,
       });
     if (error) {
       throw error;
     }
-    // Get public URL
-    const { data: urlData } = supabase.storage
-      .from("images")
-      .getPublicUrl(file.name);
-    return {
-      path: data.path,
-      url: urlData.publicUrl,
-    };
   };
 
   if (!user) {
--- src/core/Config.ts
+++ src/core/Config.ts
@@ -2,4 +2,5 @@ export const Config = {
   SupabaseUrl: "https://eovgogfkapovxpaiskjr.supabase.co",
   SupabaseKey: "sb_publishable_d5KIeIyDrzsHiBdrH8ks7w_sjV-CvpK",
   BucketName: "sotfest-images",
+  UploadFolder: "2026",
 } as const;
